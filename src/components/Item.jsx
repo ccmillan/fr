c4ed37066c09968703dfe000a02b3ee87da7ceeb
ccmillan <148159540+ccmillan@users.noreply.github.com>
2024-10-14 18:00:57 -0400

    Item Card Descripcion

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Item = () => {
+const Item = ({ producto }) => {
   return (
     <div className="producto">
       <img src={producto.imagen} alt="imagen" />
+      <div>
+        <h4>{producto.titulo}</h4>
+        <p>Precio: ${producto.precio}</p>
+        <p>Categoria: {producto.categoria}</p>
+
+        <a className="ver-mas" href={`/item/${producto.id}`}>
+          Ver mas
+        </a>
+      </div>
     </div>
   );
 };
